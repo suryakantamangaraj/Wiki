1b946578557edf96f20f0df4a48105dcc82c69ed
Surya Raj <surya.socialnetworking@gmail.com>
2023-01-15 20:10:38 +0530

    wiki update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Surya Sourcebook',
+  tagline: 'Unlocking the power of self-discovery',
   url: 'https://suryakantamangaraj.github.io',
   baseUrl: '/',
   onBrokenLinks: 'warn',
@@ -16,8 +16,8 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Surya Raj', // Usually your GitHub org/user name.
+  projectName: 'Personal Wiki', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/suryakantamangaraj/Wiki/blob/main/docs/intro.md',
         },
         blog: false,
 /*         blog: {
@@ -59,9 +59,9 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Surya Sourcebook',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Surya Sourcebook Logo',
           src: 'img/logo.svg',
         },
         items: [
