8081b864a60aea8e12a12c1cea11ff71d4d5276f
Surya Raj <60009788+suryakantamangaraj@users.noreply.github.com>
2024-08-10 19:33:48 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -127,7 +127,7 @@ const config = {
             ],
           },*/
          ],
-        copyright: `©2022 - ${new Date().getFullYear()} Surya Raj • Framed with 🤍 by Surya`,
+        copyright: `©2022 - ${new Date().getFullYear()} Surya Raj • Curated with 🤍 by Surya`,
       },
       prism: {
         theme: lightCodeTheme,
