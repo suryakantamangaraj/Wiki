1e97d07e01b96434b92aef3f70e9806389bee528
Surya Raj <60009788+suryakantamangaraj@users.noreply.github.com>
2024-08-13 01:06:26 +0000

    trying different options

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,7 @@ const config = {
         docs: {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
+          autoCollapseCategories: true,
           //exclude: ['**/any/dir/**'], //To remove sub-directories
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
